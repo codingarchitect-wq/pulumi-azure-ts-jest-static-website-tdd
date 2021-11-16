69434f68d697446a59d77bff555913736d841b8d
Alexandru-Florin Prian <alexandru-florin.prian@de.bosch.com>
2021-11-16 20:55:39 +0100

    create storage account to fulfill first test

--- index.ts
+++ index.ts
@@ -1,4 +1,12 @@
 import * as pulumi from "@pulumi/pulumi";
-import * as resources from "@pulumi/azure-native/resources";
-import * as storage from "@pulumi/azure-native/storage";
+import * as azure_native from "@pulumi/azure-native";
 
+const rg = new azure_native.resources.ResourceGroup("static-website-rg");
+
+export const storageAccount = new azure_native.storage.StorageAccount("staticwebsite", {
+    resourceGroupName: rg.name,
+    sku: {
+        name: azure_native.storage.SkuName.Standard_ZRS
+    },
+    kind: azure_native.storage.Kind.StorageV2,
+});
