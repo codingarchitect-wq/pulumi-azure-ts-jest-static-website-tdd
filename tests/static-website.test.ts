a2ca47c17c741ab2530cfaa1e7605efe8a0baec4
Alexandru-Florin Prian <alexandru-florin.prian@de.bosch.com>
2021-11-16 20:49:55 +0100

    add Pulumi Mocks to tests

---
+++ tests/static-website.test.ts
@@ -0,0 +1,13 @@
+import * as pulumi from "@pulumi/pulumi";
+
+pulumi.runtime.setMocks({
+    newResource: function(args: pulumi.runtime.MockResourceArgs): {id: string, state: any} {
+        return {
+            id: args.inputs.name + "_id",
+            state: args.inputs,
+        };
+    },
+    call: function(args: pulumi.runtime.MockCallArgs) {
+        return args.inputs;
+    },
+});
