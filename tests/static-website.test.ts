7217c7dbccc56ef73577319a7ffbc72e071e8e67
Alexandru-Florin Prian <alexandru-florin.prian@de.bosch.com>
2021-11-16 20:50:43 +0100

    load infrastructure under test

--- tests/static-website.test.ts
+++ tests/static-website.test.ts
@@ -11,3 +11,13 @@ pulumi.runtime.setMocks({
         return args.inputs;
     },
 });
+
+describe("When provisioning a static website", () => {
+    let infra: typeof import("../index");
+
+    beforeAll(async function() {
+        // It's important to import the program _after_ the mocks are defined.
+        infra = await import("../index");
+    })
+
+});
