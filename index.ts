aaf07deb3d8a7e1887074595d313e23b0e1db0a0
Alexandru-Florin Prian <alexandru-florin.prian@de.bosch.com>
2021-11-19 22:28:59 +0100

    pass test to check Storage Account Static Website should be created

--- index.ts
+++ index.ts
@@ -3,11 +3,18 @@ import * as azure_native from "@pulumi/azure-native";
 
 const rg = new azure_native.resources.ResourceGroup("static-website-rg");
 
-export const storageAccount = new azure_native.storage.StorageAccount("staticwebsite", {
+export const storageAccount = new azure_native.storage.StorageAccount("sa", {
     resourceGroupName: rg.name,
     sku: {
-        name: azure_native.storage.SkuName.Standard_ZRS
+        name: azure_native.storage.SkuName.Standard_LRS
     },
     kind: azure_native.storage.Kind.StorageV2,
     enableHttpsTrafficOnly: true
+});
+
+export const staticWebsite = new azure_native.storage.StorageAccountStaticWebsite("staticWebsite", {
+    accountName: storageAccount.name,
+    resourceGroupName: rg.name,
+    indexDocument: "index.html",
+    error404Document: "404.html",
 });
